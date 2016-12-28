fa312a38dc0b02579843c0acc386bd5e417923b1
xinin <diprieto.1992@gmail.com>
2016-12-28 17:24:32 +0100

    compilacion en dist

--- systemjs.config.js
+++ systemjs.config.js
@@ -6,6 +6,7 @@
   System.config({
     paths: {
       // paths serve as alias
+      'app': 'dist',
       'npm:': 'node_modules/'
     },
     // map tells the System loader where to look for things
@@ -20,6 +21,7 @@
       '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
       '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
       '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+      '@angular/router-deprecated': 'npm:@angular/router-deprecated/bundles/router-deprecated.umd.js',
       '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
 
@@ -30,7 +32,7 @@
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
       app: {
-        main: './main.js',
+        main: './app/main.js',
         defaultExtension: 'js'
       },
       rxjs: {
